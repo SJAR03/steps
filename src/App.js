17ab04f2841ecdee303671172e4e60a6f71eb157
SJAR03 <sayerdisrodriguez@gmail.com>
2023-09-21 16:37:51 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css'
 
-function App() {
+const message = [
+  'Learn React 👨‍💻',
+  'Apply for jobs 💼',
+  'Invest your new income 🤑'
+]
+
+export default function App () {
+  const step = 1
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className='steps'>
+      <div className='numbers'>
+        <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
+        <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
+        <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
+      </div>
 
-export default App;
+      <p className='message'>Step {step}: {message[step - 1]}</p>
+
+      <div className='buttons'>
+        <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+          Previous
+        </button>
+        <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+          Next
+        </button>
+      </div>
+    </div>
+  )
+}

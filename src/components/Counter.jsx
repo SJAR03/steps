863b7bbc716e8deb184f35ba5df5860288c3e692
72574282+SJAR03@users.noreply.github.com <72574282+SJAR03@users.noreply.github.com>
2023-09-26 00:01:28 -0600

    Logic on buttons

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,20 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Counter.css'
 
 const Counter = () => {
+  const [step, setStep] = useState(1)
+  const [count, setCount] = useState(1)
+
+  function stepCounterPlus () {
+    setStep(step + 1)
+  }
+
+  function stepCounterLess () {
+    setStep(step - 1)
+  }
+
+  function countCounterPlus () {
+    setCount(count + step)
+  }
+
+  function countCounterLess () {
+    setCount(count - step)
+  }
+
   return (
     <div>
       <h1 className='titulo'>Contador</h1>
       <div className='stepCounter'>
         <div className='counter1'>
-          <button>+</button>
-          <h4>Step: 5</h4>
-          <button>-</button>
+          <button onClick={stepCounterLess}>-</button>
+          <h4>Step: {step}</h4>
+          <button onClick={stepCounterPlus}>+</button>
         </div>
         <div className='counter2'>
-          <button>+</button>
-          <h4>Count: 10</h4>
-          <button>-</button>
+          <button onClick={countCounterLess}>-</button>
+          <h4>Count: {count}</h4>
+          <button onClick={countCounterPlus}>+</button>
         </div>
         <p>Today is Mon Jun 21 2027</p>
       </div>

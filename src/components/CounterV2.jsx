3c7f26de83d104c27729eeff6167eecc69afbafb
SJAR03 <sayerdisrodriguez@gmail.com>
2023-10-10 17:06:05 -0600

    Input range and target value

--- src/components/CounterV2.jsx
+++ src/components/CounterV2.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import './CounterV2.css'
 
 const CounterV2 = () => {
   const [count, setCount] = useState(0)
@@ -7,28 +8,47 @@ const CounterV2 = () => {
   const date = new Date('June 21 2027')
   date.setDate(date.getDate() + count)
 
+  function handleReset () {
+    setCount(0)
+    setStep(1)
+  }
+
   return (
     <div>
-      <div>
-        <button onClick={() => setStep(s => step - 1)}>-</button>
-        <span>Step: {step}</span>
-        <button onClick={() => setStep(s => step + 1)}>+</button>
-      </div>
-      <div>
-        <button onClick={() => setCount(c => count - step)}>-</button>
-        <span>Count: {count}</span>
-        <button onClick={() => setCount(c => count + step)}>+</button>
-      </div>
 
-      <p>
-        <span>{count === 0
-          ? 'Today is '
-          : count > 0
-            ? `${count} days from today is `
-            : `${Math.abs(count)} days ago was `}
-        </span>
-        <span>{date.toDateString()}</span>
-      </p>
+      <h1 className='title'>Titulo</h1>
+      <div className='counter'>
+        <div>
+
+          <input type='range' min='1' max='10' className='range-line' value={step} onChange={e => setStep(Number(e.target.value))} />
+
+          <span>Step: {step}</span>
+        </div>
+        <div>
+          <button onClick={() => setCount(c => count - step)}>-</button>
+          <input type='text' value={count} onChange={e => setCount(Number(e.target.value))} />
+          <button onClick={() => setCount(c => count + step)}>+</button>
+        </div>
+
+        <p>
+          <span>{count === 0
+            ? 'Today is '
+            : count > 0
+              ? `${count} days from today is `
+              : `${Math.abs(count)} days ago was `}
+          </span>
+          <span>{date.toDateString()}</span>
+        </p>
+        {(count !== 0 || step !== 1)
+          ? (
+            <div>
+              <button onClick={handleReset}>
+                Reset
+              </button>
+            </div>
+            )
+          : null}
+      </div>
     </div>
   )
 }
